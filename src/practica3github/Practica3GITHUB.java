0e5e87751e9dec07fbc77a4337fd9de5b383533e
JaviQuilumba <javier0611quilumba@gmail.com>
2024-04-23 18:49:39 -0500

    practica 3 suma

---
+++ src/practica3github/Practica3GITHUB.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3github;
+
+/**
+ *
+ * @author JAVIER
+ */
+public class Practica3GITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=5;
+        int b=6;
+        int suma=0;
+        suma= a+b;
+        System.out.print("Suma:" + suma);
+    }
+    
+}
